8a46b2dfd425aa9411c54ee622e0b47c46d81dcc
Subash Rajaram <subashrajaram2005@gmail.com>
2025-08-04 18:49:24 +0530

    Create multiply without * operator.java

---
+++ looping/multiply without * operator.java
@@ -0,0 +1,31 @@
+/*Problem Name
+Multiply Two Numbers Without Using * Operator
+
+Problem Statement
+Write a C program to multiply the given two numbers without using * operator.
+
+Input Format
+Integer Integer
+
+Constraints
+Nill
+
+Output Format
+Integer
+
+Sample Test Cases
+Sample Input 0
+10 2
+
+Sample Output 0
+20
+*/
+import java.util.*; 
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a * b);
+    }
+}
