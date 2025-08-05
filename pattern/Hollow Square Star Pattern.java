c1e6f3cd4b4e160a2aea000cd41a0423fd5fb151
Subash Rajaram <subashrajaram2005@gmail.com>
2025-08-05 15:46:07 +0530

    Create Hollow Square Star Pattern.java

---
+++ pattern/Hollow Square Star Pattern.java
@@ -0,0 +1,47 @@
+/*
+Problem Name
+Print Hollow Square Star Pattern
+
+Problem Statement
+Write a program to display the following hollow square star pattern.
+
+Input Format
+Integer
+
+Constraints
+Input > 0
+
+Output Format
+Star pattern
+
+Sample Test Cases
+Sample Input 0
+5
+
+Sample Output 0
+
+
+*****  
+*   *  
+*   *  
+*   *  
+*****
+
+*/
+
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(i == 1 || i == n || j == 1 || j == n)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
