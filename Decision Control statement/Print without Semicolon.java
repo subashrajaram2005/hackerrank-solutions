41b2145b109e321615ec8667701022e05558d0ae
Subash Rajaram <subashrajaram2005@gmail.com>
2025-08-05 15:17:50 +0530

    Create Print without Semicolon.java

---
+++ Decision Control statement/Print without Semicolon.java
@@ -0,0 +1,27 @@
+/*Problem Name
+Print "Hello World" Without Semicolon
+
+Problem Statement
+Print the statement "Hello World" using printf without using a semicolon (;).
+
+Input Format
+NIL
+
+Constraints
+NIL
+
+Output Format
+Hello World
+
+Sample Test Cases
+Sample Output 0
+Hello World
+
+*/
+
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        if(System.out.printf("Hello World") != null) {}
+    }
+}
